d8c6dea561bb3036a4c4bf3aaab966a21578d72b
Harshit yadav <98582750+Harshit00yadav@users.noreply.github.com>
2024-12-12 18:29:41 +0530

    Update GamePanel.java

--- com/company/game/GamePanel.java
+++ com/company/game/GamePanel.java
@@ -82,7 +82,7 @@ public class GamePanel extends JPanel implements Runnable{
       }
 
       if (timer >= 1000000000){
-        System.out.println("FPS : " + drawCount);
+        System.out.print("\rFPS : " + drawCount);
         drawCount = 0;
         timer = 0;
       }
